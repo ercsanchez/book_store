d003489f2bfc324bfa3753d725b376910e7a3292
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-05 22:13:25 +0000

    91. add Author Model and link to Book Model using a ForeignKey in Author model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -6,12 +6,18 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(max_length=50, null=True)
+    # author = models.CharField(max_length=50, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestseller = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
