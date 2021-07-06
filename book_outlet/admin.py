fe0838ec58cf04410d1682cfd61c7d3c8a27fd0f
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-07-06 03:01:11 +0000

    100. register Country Model in admin | set Meta class to display correct plural form of Country | set __str__ of Country

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Address, Book, Author
+from .models import Address, Book, Author, Country
 
 # Register your models here.
 
@@ -14,3 +14,4 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 admin.site.register(Address)
+admin.site.register(Country)
